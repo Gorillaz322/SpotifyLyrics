fba8184723bf73c70711eccf763892306e5b4fe7
Vadim.Zhyvylo <jyvylo5@gmail.com>
2018-02-25 05:34:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# SpotifyLyrics
+## About
+
+Simple python script for getting lyric for current Spotify song.<br />
+Lyric will be saved as a text file to **lyrics** folder in script directory.<br />
+Lyric is displayed by **vim** text editor.
+
+For convenience, you can make shortcut.
+
+Tested only on Ubuntu.
+## Requirements
+
+For getting current Spotify playback [playerctl](https://github.com/acrisci/playerctl) is used
---
+++ get_lyric.py
@@ -0,0 +1,107 @@
+import urllib2
+import json
+import subprocess
+import os
+import re
+
+location = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+if not os.path.exists('lyrics'):
+    os.makedirs('lyrics')
+
+base_path = location + '/lyrics'
+
+
+class NoDataFoundException(Exception):
+    pass
+
+
+class Lyric(object):
+    def __init__(self, artist, title):
+        self.artist = artist
+        self.title = title
+        self.filename = "{artist}-{song}.txt"\
+            .format(artist="_".join(self.artist.split()),
+                    song="_".join(self.title.split()))
+
+    @staticmethod
+    def _urlify_artist(artist):
+        return artist \
+            .replace(' ', '%20') \
+            .replace('/', '%2F')
+
+    @staticmethod
+    def _urlify_song(title):
+        # remove regular brackets
+        title = re.sub(r'\([^)]*\)', '', title)
+
+        # remove square brackets
+        title = re.sub(r'\[.*?\]', '', title)
+
+        # remove everything after a dash
+        if ' - ' in title:
+            title = re.sub(r'\ - .*$', '', title)
+
+        return title \
+            .strip() \
+            .replace(' ', '%20') \
+            .replace('/', '%2F')
+
+    def get(self):
+        # if lyric was already saved to file - getting from it
+        if os.path.isfile(base_path + self.filename):
+            with open(base_path + self.filename, 'r') as lyrics:
+                lyric = lyrics.read()
+        else:
+            response = json.load(urllib2.urlopen(
+                'http://lyric-api.herokuapp.com/api/find/{artist}/{song_title}'
+                    .format(artist=self._urlify_artist(artist),
+                            song_title=self._urlify_song(title))
+            ))
+
+            if response['err'] != 'none':
+                raise NoDataFoundException()
+
+            lyric = response['lyric']
+
+            f = open(base_path + self.filename, 'w')
+            f.write(lyric)
+
+        return lyric
+
+    def show(self):
+        lyric_text = self.get()
+        lines = lyric_text.split('\n')
+
+        # length of terminal window as number of lines in a text
+        y_size = len(lines) + 1
+
+        # width of terminal as longest line in a text
+        x_size = max([len(line) for line in lines])
+
+        subprocess.call(
+            [
+                'gnome-terminal',
+                '--geometry',
+                '{x}x{y}+4096+0'.format(x=str(x_size), y=str(y_size)),
+                '-e',
+                'vim "{file}"'.format(file=base_path + self.filename)
+            ]
+        )
+
+artist = subprocess.check_output(['playerctl', 'metadata', 'artist'])
+title = subprocess.check_output(['playerctl', 'metadata', 'title'])
+lyric = Lyric(artist, title)
+
+try:
+    lyric.show()
+except NoDataFoundException:
+    subprocess.call([
+        "notify-send",
+        "-i",
+        "dialog-error",
+        "Cannot find lyric for ",
+        "{} - {}".format(artist, title)
+    ])
+
